2354382c1c0529b84b9b4dc88c32c3360b83abfe
Iliyan Germanov <iliyan.germanov971@gmail.com>
2026-04-24 01:06:33 +0300

    Add sops-nix input

--- flake.nix
+++ flake.nix
@@ -11,9 +11,13 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixvim, disko, ... }:
+  outputs = { self, nixpkgs, nixvim, disko, sops-nix, ... }:
     let
       eachSystem =
         f:
@@ -83,6 +87,7 @@
           system = "x86_64-linux";
           modules = [
             disko.nixosModules.disko
+            sops-nix.nixosModules.sops
             ./hosts/lenovo-old/disk-config.nix
             ./hosts/lenovo-old/configuration.nix
           ];
