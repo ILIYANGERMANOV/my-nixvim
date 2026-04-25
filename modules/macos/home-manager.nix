a60525cb97911ff2dccf0b8af0b9423eb127cac9
iliyangermanov <iliyan.germanov971@gmail.com>
2026-04-25 23:22:54 +0300

    Apply fix

--- modules/macos/home-manager.nix
+++ modules/macos/home-manager.nix
@@ -1,6 +1,7 @@
 { inputs, root, config, ... }: {
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "before-home-manager";
   home-manager.sharedModules = [ inputs.nixvim.homeModules.nixvim ];
   home-manager.extraSpecialArgs = {
     inherit root;
