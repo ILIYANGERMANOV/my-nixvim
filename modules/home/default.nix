038d988f48c735ca27364a3451c2e081a154d0ca
iliyangermanov <iliyan.germanov971@gmail.com>
2026-04-27 20:18:48 +0300

    Fix direnv error

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -26,6 +26,7 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    package = pkgs.direnv.overrideAttrs (_: { doCheck = false; });
   };
 
   home.packages = with pkgs; [
